c8512b915c1b7e888eeb5521224df49520fd439d
ArtMGreen <artyom5145@gmail.com>
2022-01-16 17:16:39 +0300

    Game class, working GUI

--- field.py
+++ field.py
@@ -15,7 +15,7 @@ class Tile:
 class Field:
     def __init__(self, size):
         self.size = size
-        self.table = [[Tile()] * size for _ in range(size)]
+        self.table = [[Tile() for c1 in range(size)] * size for _ in range(size)]
 
     def toggle_tile(self, x, y):
         if self.table[y][x].is_alive():
@@ -29,6 +29,9 @@ class Field:
     def set_tile_dead(self, x, y):
         self.table[y][x].set_dead()
 
+    def get_tile_state(self, x, y):
+        return self.table[y][x].is_alive()
+
     def count_living_neighbours(self, x, y):
         res = self.table[(y - 1) % self.size][(x - 1) % self.size].is_alive() + \
               self.table[(y - 1) % self.size][x].is_alive() + \
@@ -41,7 +44,7 @@ class Field:
         return res
 
     def update(self):
-        numeral_print = [[0] * self.size for _ in self.size]
+        numeral_print = [[0] * self.size for _ in range(self.size)]
         for x in range(self.size):
             for y in range(self.size):
                 neighbours = self.count_living_neighbours(x, y)
@@ -49,6 +52,8 @@ class Field:
                     numeral_print[y][x] = 1
                 elif neighbours < 2 or neighbours > 3:
                     numeral_print[y][x] = 0
+                else:
+                    numeral_print[y][x] = self.get_tile_state(x, y)
         for x in range(self.size):
             for y in range(self.size):
                 if numeral_print[y][x]:
